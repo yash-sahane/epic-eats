2527425ebbc49c461934ac6fbb4b37545a8ca746
Yash <yashsahane23@gmail.com>
2024-06-06 18:40:42 +0530

    server update client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,8 @@ import App from './App.jsx'
 import './index.css'
 import { StoreContextProvider } from './context/StoreContext.jsx'
 
-export const SERVER_URI = 'http://localhost:5000'
+// export const SERVER_URI = 'http://localhost:5000'
+export const SERVER_URI = 'https://epic-eats-mfec.onrender.com'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StoreContextProvider>
