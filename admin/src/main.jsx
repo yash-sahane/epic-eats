47a0602e25dd7ceea769fc18e825d4505034a0fc
Yash <yashsahane23@gmail.com>
2024-06-06 18:36:03 +0530

    server update

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-export const SERVER_URI = 'http://localhost:5000'
+// export const SERVER_URI = 'http://localhost:5000'
+export const SERVER_URI = 'https://epic-eats-mfec.onrender.com'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
