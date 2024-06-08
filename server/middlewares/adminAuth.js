0bb20209e922567b74ee308fc96802eda17843b4
Yash <yashsahane23@gmail.com>
2024-06-08 23:58:24 +0530

    console cleaner

--- server/middlewares/adminAuth.js
+++ server/middlewares/adminAuth.js
@@ -2,7 +2,7 @@ import ErrorHandler from "./error.js";
 
 const isAdminAuthenticated = async (req, res, next) => {
   const user = req.user;
-  console.log(user);
+  // console.log(user);
   if (user.role !== 'admin') {
     return next(new ErrorHandler('User must have admin privileges', 400));
   }
