c6efd613db59589cfd870824dbdf880b8d838a10
Yash Sahane <yashsahane23@gmail.com>
2024-09-10 19:39:24 +0530

    [Fix] Search UI fix

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -49,17 +49,43 @@ const Header = ({ setAuthPopup }) => {
           </ul>
         </div>
         <div className='flex items-center gap-6 msm:gap-8'>
-          <div className='flex items-center p-1 bg-white shadow-md rounded-full transition-all'>
-            <input type="text" className={`outline-none px-2 transition-all duration-300 text-sm ${!isSearchActive ? 'w-0 px-0' : ''}`} placeholder='Search food items...' onChange={(e) => searchHandler(e.target.value)} value={searchInput} />
-            <img src={search_icon2} alt="search_icon" className='w-[32px] cursor-pointer p-2 bg-[#f03e22] rounded-full' onClick={() => { setSearchInput(''); setisSearchActive(prev => !prev) }} />
+          <div className={`flex items-center p-1 bg-white shadow-md rounded-full transition-all duration-300 ease-in-out ${isSearchActive ? 'w-full' : ''}`}>
+            <input
+              type="text"
+              placeholder='Search food items...'
+              className={`outline-none text-sm transition-width duration-300 ease-in-out ${isSearchActive ? 'w-48 px-2' : 'w-0'}`}
+              onChange={(e) => searchHandler(e.target.value)}
+              value={searchInput}
+            />
+            <img
+              src={search_icon2}
+              alt="search_icon"
+              className='w-8 cursor-pointer p-2 bg-[#f03e22] rounded-full'
+              onClick={() => { setSearchInput(''); setisSearchActive(prev => !prev) }}
+            />
           </div>
-          {token && <div className='relative w-[20px] cursor-pointer'><img src={basket_icon} alt="basket_icon" onClick={() => navigate('/cart')} /><div className='absolute -top-3 -right-3 bg-primary h-6 w-6 rounded-full flex justify-center items-center'><p className='text-white text-sm'>{noOfItems}</p></div></div>}
-          {!token && <div><button className='py-2 px-6 xl:py-3 xl:px-7 rounded-full border border-gray-500' onClick={() => setAuthPopup('login')}>Sign In</button></div>}
-          {token && <div className='group w-[20px] relative cursor-pointer'><img src={profile_icon} alt="profile_icon" />
-            <div className='customer-dropdown pt-2 hidden absolute top-[22px] right-0 overflow-hidden group-hover:flex'>
-              <div className='flex-col w-[120px] text-sm items-center cursor-pointer border border-primary bg-[#fef3ee] rounded-md'>
-                <div className='hover:text-primary flex gap-2 items-center w-full justify-center py-2' onClick={() => navigate('/orders')}><img src={bag_icon} className='w-6' alt="bag_icon" /><p>Orders</p></div>
-                <div className='hover:text-primary flex gap-2 items-center w-full justify-center py-2' onClick={logoutHandler}><img src={logout_icon} className='w-6' alt="bag_icon" /><p>Logout</p></div>
+          {!token && <div>
+            <button className='py-2 px-6 xl:py-3 xl:px-7 rounded-full border border-gray-500' onClick={() => setAuthPopup('login')}>Sign In</button>
+          </div>}
+          {token && <div className='flex msm:gap-8 gap-6'>
+            <div className='relative w-[20px] cursor-pointer'>
+              <img src={basket_icon} alt="basket_icon" onClick={() => navigate('/cart')} />
+              <div className='absolute -top-3 -right-3 bg-primary h-6 w-6 rounded-full flex justify-center items-center'>
+                <p className='text-white text-sm'>{noOfItems}</p>
+              </div>
+            </div> <div className='group w-[20px] relative cursor-pointer'>
+              <img src={profile_icon} alt="profile_icon" />
+              <div className='customer-dropdown pt-2 hidden absolute top-[22px] right-0 overflow-hidden group-hover:flex'>
+                <div className='flex-col w-[120px] text-sm items-center cursor-pointer border border-primary bg-[#fef3ee] rounded-md'>
+                  <div className='hover:text-primary flex gap-2 items-center w-full justify-center py-2' onClick={() => navigate('/orders')}>
+                    <img src={bag_icon} className='w-6' alt="bag_icon" />
+                    <p>Orders</p>
+                  </div>
+                  <div className='hover:text-primary flex gap-2 items-center w-full justify-center py-2' onClick={logoutHandler}>
+                    <img src={logout_icon} className='w-6' alt="bag_icon" />
+                    <p>Logout</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>}
