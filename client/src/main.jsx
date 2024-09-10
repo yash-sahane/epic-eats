932caa3566a8b64f9243b0879458d0e980ddc51f
Yash Sahane <yashsahane23@gmail.com>
2024-09-10 19:41:56 +0530

    [Update] SERVER_URI fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,8 @@ import App from './App.jsx'
 import './index.css'
 import { StoreContextProvider } from './context/StoreContext.jsx'
 
-export const SERVER_URI = 'http://localhost:5000'
-// export const SERVER_URI = 'https://epic-eats-mfec.onrender.com'
+// export const SERVER_URI = 'http://localhost:5000'
+export const SERVER_URI = 'https://epic-eats-mfec.onrender.com'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StoreContextProvider>
