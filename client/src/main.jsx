c94e68158ece41e5074ac83565d369c53669ec9c
Yash Sahane <yashsahane23@gmail.com>
2025-01-03 21:04:21 +0530

    [Fix] Cron

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,13 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { StoreContextProvider } from './context/StoreContext.jsx'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { StoreContextProvider } from "./context/StoreContext.jsx";
 
-// export const SERVER_URI = 'http://localhost:5000'
-export const SERVER_URI = 'https://epic-eats-mfec.onrender.com'
+// export const SERVER_URI = "http://localhost:5000";
+export const SERVER_URI = "https://epic-eats-mfec.onrender.com";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StoreContextProvider>
     <App />
   </StoreContextProvider>
-)
+);
--- server/app.js
+++ server/app.js
@@ -1,33 +1,33 @@
-import { config } from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-import foodRouter from './routes/Food.js';
-import userRouter from './routes/User.js';
-import cartRouter from './routes/Cart.js';
-import orderRouter from './routes/Order.js';
-import { errMiddleware } from './middlewares/error.js';
-import job from './cron/cron.js';
+import { config } from "dotenv";
+import express from "express";
+import cors from "cors";
+import foodRouter from "./routes/Food.js";
+import userRouter from "./routes/User.js";
+import cartRouter from "./routes/Cart.js";
+import orderRouter from "./routes/Order.js";
+import { errMiddleware } from "./middlewares/error.js";
+import job from "./cron/cron.js";
 
 export const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-job.start();
+// job.start();
 
 config({
-  path: './database/config.env'
-})
+  path: "./database/config.env",
+});
 
 // routes
-app.use('/api/food/', foodRouter);
-app.use('/api/user/', userRouter);
-app.use('/api/cart/', cartRouter);
-app.use('/api/order/', orderRouter);
-app.use('/images', express.static('uploads'));
+app.use("/api/food/", foodRouter);
+app.use("/api/user/", userRouter);
+app.use("/api/cart/", cartRouter);
+app.use("/api/order/", orderRouter);
+app.use("/images", express.static("uploads"));
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.use(errMiddleware);
